ce098c9c6c3d4283536b5e5cec672154b367f710
Ana Jéssica <jessicamoura@gmail.com>
2026-08-27 17:44:13 -0300

    Criação do rodapé, tela de funcionamento e inicio dos models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    imagem = models.ImageField()
+    titulo = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.titulo
--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Card
 
 def home(request):
     return render(request, 'app/home.html')
+
+    card = Card.objects.all()
+
+    return render(request, "app/home.html", {
+        'card': card
+    })
 
 def login(request):
     return render(request, 'app/login.html')
