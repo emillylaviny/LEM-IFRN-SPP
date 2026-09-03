7a26c2af3a3079c5c076575cdde76b9bb08c5e75
Ana jéssica <jessicamoura0355@gmail.com>
2026-09-03 15:15:24 -0300

    iniciando cruds básicos

--- app/models.py
+++ app/models.py
@@ -7,3 +7,31 @@ class Card(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class cadastro_usuario (models.Model):
+    nome = models.CharField(max_length=300)
+    apelido = models.CharField(max_length=300)
+    cpf = models.CharField(max_length=14)
+    data_nascimento = models.DateField(auto_now=True)
+    foto = models.ImageField(upload_to="")
+    escola = models.CharField(max_length=150)
+    declaracao_escola = models.FileField()
+    cidade_escola = models.CharField(max_length=150)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=150)
+    cidade_residencia = models.CharField(max_length=150)
+    senha = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+class login (models.Model):
+    usuario_cpf = models.CharField(max_length=14)
+    senha = models.CharField(max_length=150)
+    
+class login (models.Model):
+    usuario_cpf = models.CharField(max_length=14)
+    senha = models.CharField(max_length=150)
+
+class Cadastro_materiais (models.Model):
+    pass
