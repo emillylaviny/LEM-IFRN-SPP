7222dc6c26397fe3f56f4a2efcc2815bebf12ae0
Nathalia Cordeiro <nathaliamilenic@gmail.com>
2026-09-21 21:08:30 -0300

    add forms

--- LEM/urls.py
+++ LEM/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='LEM/login.html'),
+        name='login'
+    ),
 ]
